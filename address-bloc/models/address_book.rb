3f9111738e7b0b1bfa41daf4700ba623bc497267
Heda Hahn <hedassecret@gmail.com>
2015-11-17 08:55:58 -0800

    New address book entries

--- address-bloc/models/address_book.rb
+++ address-bloc/models/address_book.rb
@@ -1,59 +1,40 @@
-<<<<<<< HEAD
-require_relative 'entry.rb'
-=======
 require_relative 'entry'
->>>>>>> ff6d84085f58375fda02402275d65eb2c9f700c2
+require "csv"
 
 class AddressBook
   attr_accessor :entries
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ff6d84085f58375fda02402275d65eb2c9f700c2
   def initialize
      @entries = []
    end
 
-<<<<<<< HEAD
    def remove_entry(name, phone, email)
      delete_entry = nil
      @entries.each do |entry|
    end
-
-   @entries.delete(delete_entry)
-
- end
+    @entries.delete(delete_entry)
+  end
 
 
  def add_entry(name, phone_number, email)
-
      index = 0
      @entries.each do |entry|
-
        if name < entry.name
          break
        end
        index += 1
      end
-
      @entries.insert(index, Entry.new(name, phone_number, email))
    end
 
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+   end
+
+  end
 end
-=======
-   def add_entry(name, phone_number, email)
-  # #9
-      index = 0
-      @entries.each do |entry|
-  # #10
-        if name < entry.name
-          break
-        end
-        index += 1
-      end
-  # #11
-      @entries.insert(index, Entry.new(name, phone_number, email))
-    end
- end
->>>>>>> ff6d84085f58375fda02402275d65eb2c9f700c2
