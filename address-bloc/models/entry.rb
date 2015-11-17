61819b31dc7c940b5ad9716c729794efa3a9132b
Heda Hahn <hedassecret@gmail.com>
2015-11-17 09:59:16 -0800

    new entry.rb

--- address-bloc/models/entry.rb
+++ address-bloc/models/entry.rb
@@ -1,5 +1,4 @@
 class Entry
-<<<<<<< HEAD
 
    attr_accessor :name, :phone_number, :email
 
@@ -13,17 +12,4 @@ class Entry
      "Name: #{@name}\nPhone Number: #{@phone_number}\nEmail: #{@email}"
    end
 
-=======
-  attr_accessor :name, :phone_number, :email
-
-  def initialize(name, phone_number, email)
-     @name = name
-     @phone_number = phone_number
-     @email = email
-  end
-
-  def to_s
-     "Name: #{@name}\nPhone Number: #{@phone_number}\nEmail: #{@email}"
-   end
->>>>>>> ff6d84085f58375fda02402275d65eb2c9f700c2
 end
